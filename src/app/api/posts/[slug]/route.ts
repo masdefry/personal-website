a4b1ef7caa958a8151eb6ebae6b584c323978cb2
masdefry <muhammaddefryan@gmail.com>
2025-03-26 20:21:33 +0700

    fix:route.ts

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -1,16 +1,25 @@
-import { NextRequest, NextResponse  } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import Backendless from '../../../../lib/backendless';
 
-export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
+export async function GET(req: NextRequest) {
   try {
-    const { slug } = params;
-    const getPosts = await Backendless.Data.of('Posts').find({where: `slug = '${slug}'`});
-    
-    if (getPosts.length === 0) {
-      return NextResponse.json({message: `Post with Slug = ${slug} not found`}, {status: 404});
+    const slug = req.nextUrl.pathname.split('/').pop(); 
+
+    if (!slug) {
+      return NextResponse.json({ message: 'Slug is required' }, { status: 400 });
     }
-    return NextResponse.json({message: `Get Post with Slug = ${slug} Success`, data: getPosts}, {status: 200});
+
+    const getPosts = await Backendless.Data.of('Posts').find({ where: `slug = '${slug}'` });
+
+    if (getPosts.length === 0) {
+      return NextResponse.json({ message: `Post with Slug = ${slug} not found` }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: `Get Post with Slug = ${slug} Success`, data: getPosts },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({message: `Get Post Failed`, error}, {status: 500});
+    return NextResponse.json({ message: `Get Post Failed`, error }, { status: 500 });
   }
 }
