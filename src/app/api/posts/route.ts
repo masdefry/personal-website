70bd5a6083655d6dd240b05cb5193d812c1167c0
masdefry <muhammaddefryan@gmail.com>
2025-03-27 13:50:35 +0700

    fix:slug on route.js

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,22 +1,60 @@
-import { NextRequest, NextResponse  } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import Backendless from '../../../lib/backendless';
+
+function generateSlug(title: string) {
+  return title
+    .normalize('NFD')
+    .replace(/[^\w\s-]/g, '')
+    .trim()
+    .replace(/\s+/g, '-')
+    .toLowerCase();
+}
 
 export async function POST(req: NextRequest) {
   try {
     const { title, content, author, imageUrl, description } = await req.json();
-    await Backendless.Data.of('Posts').save({ title, content, author, imageUrl, description, slug: title.split(' ').join('-') });
-    return NextResponse.json({message: 'Create Post Successfully', data: { title, content, author, imageUrl, slug: title.split(' ').join('-') }}, {status: 201});
+    const slug = generateSlug(title);
+    await Backendless.Data.of('Posts').save({
+      title,
+      content,
+      author,
+      imageUrl,
+      description,
+      slug,
+    });
+    return NextResponse.json(
+      {
+        message: 'Create Post Successfully',
+        data: {
+          title,
+          content,
+          author,
+          imageUrl,
+          slug: title.split(' ').join('-'),
+        },
+      },
+      { status: 201 }
+    );
   } catch (error) {
-    return NextResponse.json({message: 'Create Post Failed', error}, {status: 500});
+    return NextResponse.json(
+      { message: 'Create Post Failed', error },
+      { status: 500 }
+    );
   }
 }
 
 export async function GET() {
   try {
     const getPosts = await Backendless.Data.of('Posts').find();
-    
-    return NextResponse.json({message: 'Get Posts Success', data: getPosts}, {status: 200});
+
+    return NextResponse.json(
+      { message: 'Get Posts Success', data: getPosts },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({message: 'Get Posts Failed', error}, {status: 500});
+    return NextResponse.json(
+      { message: 'Get Posts Failed', error },
+      { status: 500 }
+    );
   }
 }
